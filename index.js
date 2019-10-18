831db61300566e9bbd91a1a3e4258677c4aafe31
Robert Heler <robertheler@gmail.com>
2019-10-18 21:34:19 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 function theBeatlesPlay (musicians, instruments) {
   var array = [];
   for (var i = 0; i < musicians.length; i++) {
-    var newPair = musicians[i] + " plays " insturments[0];
+    var newPair = musicians[i] + " plays " instruments[0];
     array.push(newPair);
   }
   
